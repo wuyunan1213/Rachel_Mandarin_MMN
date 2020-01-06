f20f45c853aadb96782c1eaee5c5c74ca35dc057
Charles Wu <charleswu.01@gmail.com>
2020-01-05 23:07:31 -0500

    new chnages

--- VoiceToneMMN.m
+++ VoiceToneMMN.m
@@ -2,7 +2,7 @@
 %Code to collect MMN data for Rachel's Honor's Thesis
 %Created by MD on 12/8/2019
 
-%LAST EDIT on 1/5/2019 by Charles
+%LAST EDIT on 1/5/2019 by Charles Wu at 11:05pm
 
 %% INITIALIZATION
 close all;
